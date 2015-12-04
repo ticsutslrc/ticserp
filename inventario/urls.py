b1cb7d554858934b9d7a4b4676d66a78e01202b3
Luis Iturrios <luis@sait.mx>
2015-12-04 09:04:30 -0700

    Borrar vistas incorrectas

--- inventario/urls.py
+++ inventario/urls.py
@@ -7,7 +7,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
-    url(r'^productos$', views.ProductoConsultar, name='productos_consultar'),
-    url(r'^productos/alta$', views.ProductoAlta, name='productos_alta'),
-    url(r'^productos/(?P<pk>[0-9]+)$', views.ProductoModificar, name='productos_modificar'),
 ]
